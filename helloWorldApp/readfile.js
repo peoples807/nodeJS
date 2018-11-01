39269ad6c717fc0a749eae59f96cd5706c1f82d8
chidi79 <egusiboy79@gmail.com>
2018-11-01 11:21:38 +0100

    initial commit: helloWorld

---
+++ helloWorldApp/readfile.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var options = {encoding: "utf-8"};
+fs.readFile("C:\\Users\\ChidiIfediora\\Dev\\Data\\stockquote_json.txt", options, function(err, data){
+    if(err){
+        console.log("There was an error reading the file."+err.message);
+        return;
+    }
+    var targetChar = data.match(/t/gi);
+    //console.log(targetChar);
+    if(targetChar == null)
+        console.log("Character not found.");
+    else
+        console.log(targetChar.length + " occurences were found.");
+})
+
+console.log("Does this come first?");
